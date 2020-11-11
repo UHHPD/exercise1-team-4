75a9272653e3f753132f0bcaf36ab45b1b3ae4a7
j0nbr4 <jonathan-bracker@hotmail.com>
2020-11-11 11:40:19 +0000

    concept was renewed

--- summieren.cc
+++ summieren.cc
@@ -1,18 +1,21 @@
 /* This program computes the line-wise sum of the data stored in daten.txt and writes the results into a new file datensumme.txt*/
 
 #include <iostream>
-#include <string>
-#include <sstream>
 #include <fstream>
 
-int main(){
-  std::string line;
-  std::ifstream daten("daten.txt");
-  std::ofstream datensumme("datensumme.txt");
+int a;
+int b;
+int sum;
 
-  while (getline(daten, line)){
-    datensumme << line[0] + line[2] << std::endl;
+int main(){
+  std::ifstream fin("daten.txt");
+  std::ofstream fout("datensumme.txt");
+
+  for(int i=0; i<234; i++){
+    fin >> a >> b;
+    fout << a+b << std::endl;
   }
-  daten.close();
-  datensumme.close();
+  
+  fin.close();
+  fout.close();
 }
