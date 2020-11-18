6c578f64d58fbf783d68e13d949f2ba879d7f825
Laura Büttgen <laura.buettgen@gmail.com>
2020-11-18 16:29:59 +0000

    Aufgabe 2b, Blatt 2

--- read.cc
+++ read.cc
@@ -3,6 +3,16 @@
 #include <string>
 #include <cmath>
 
+
 int main(){
-  std::ifstream
+  int a[8];
+  int * point = a;
+  std::ifstream data("datensumme.txt");
+
+  while(!data.eof()){
+    data.read(point, 9);
+    cout << *point;
+
+
+  }
 }
